5f74ec5106e4b33dd5a5f315658e532e96d33d0d
slabaniego <133892057+slabaniego@users.noreply.github.com>
2023-08-19 10:27:40 -0400

    Update Deck.java

--- Fundamentals of Software Design & Development/CardGame/src/cardgame/Deck.java
+++ Fundamentals of Software Design & Development/CardGame/src/cardgame/Deck.java
@@ -12,7 +12,7 @@ public class Deck {
 
     private void initializeDeck() {
         String[] colors = {"Red", "Blue", "Green", "Yellow"};
-        String[] values = {"0", "1", "2", /* ... */}; // Fill in the card values
+        String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "draw_2", "reverse", "skip", "wild", "wild_draw_4"};
         
         for (String color : colors) {
             for (String value : values) {
